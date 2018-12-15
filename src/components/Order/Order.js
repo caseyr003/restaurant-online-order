58870e182973fe11738b93ca87db75236720649e
Casey Ross <caseyross003@gmail.com>
2018-12-15 10:57:58 -0600

    add OrderItem component

---
+++ src/components/Order/Order.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Order.module.css';
+
+const order = (props) => (
+  <div>Order</div>
+);
+
+export default order;
---
+++ src/components/Order/OrderItem/OrderItem.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './OrderItem.module.css';
+
+class OrderItem extends Component {
+  render() {
+    let item = null;
+
+    switch(this.props.type) {
+      case('bread-bottom'):
+        item=<div className={styles.BreadBottom}></div>;
+        break;
+      case('bread-top'):
+        item=(
+          <div className={styles.BreadTop}>
+            <div className={styles.Seeds1}></div>
+            <div className={styles.Seeds2}></div>
+          </div>
+        );
+        break;
+      case('meat'):
+        item=<div className={styles.Meat}></div>;
+        break;
+      case('cheese'):
+        item=<div className={styles.Cheese}></div>;
+        break;
+      case('salad'):
+        item=<div className={styles.Salad}></div>;
+        break;
+      case('bacon'):
+        item=<div className={styles.Bacon}></div>;
+        break;
+      default:
+          item=null;
+    };
+
+    return item;
+  }
+};
+
+OrderItem.propTypes = {
+  type: PropTypes.string.isRequired
+};
+
+export default OrderItem;
