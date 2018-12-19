4b9215aaf86a52afd65a97be6cad0d85126bb8fc
Casey Ross <caseyross003@gmail.com>
2018-12-19 16:10:13 -0600

    fix order summary to only rerender when modal is shown

--- src/components/Order/OrderSummary/OrderSummary.js
+++ src/components/Order/OrderSummary/OrderSummary.js
@@ -1,35 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const itemsSummary = Object.keys(props.items)
-      .map(key => {
-        return (
-          <li key={key}>
-            <span style={{textTransform:'capitalize'}}>
-              {key}
-            </span>: {props.items[key]}
-          </li>
-        );
-      });
-  return (
-    <Aux>
-      <h3>Your Order Summary</h3>
-      <p>Thanks for your order! We hope you enjoy your items</p>
-      <ul>
-        {itemsSummary}
-      </ul>
-      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button 
-        clicked={props.purchaseCancelled}
-        type="Danger">Cancel</Button>
-      <Button 
-        clicked={props.purchaseConfirmed}
-        type="Success">Checkout</Button>
-    </Aux>
-  );
-};
+class OrderSummary extends Component {
+  // could have been left as a functional component
+  componentWillUpdate() {
+    console.log("Order Summary will update");
+  }
 
-export default orderSummary;
+  render () {
+    const itemsSummary = Object.keys(this.props.items)
+    .map(key => {
+      return (
+        <li key={key}>
+          <span style={{textTransform:'capitalize'}}>
+            {key}
+          </span>: {this.props.items[key]}
+        </li>
+      );
+    });
+
+    return (
+      <Aux>
+        <h3>Your Order Summary</h3>
+        <p>Thanks for your order! We hope you enjoy your items</p>
+        <ul>
+          {itemsSummary}
+        </ul>
+        <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button 
+          clicked={this.props.purchaseCancelled}
+          type="Danger">Cancel</Button>
+        <Button 
+          clicked={this.props.purchaseConfirmed}
+          type="Success">Checkout</Button>
+      </Aux>
+    );
+  }
+}
+
+export default OrderSummary;
